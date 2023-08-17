522fe23c62b6a5056b518235a068bcb9a6fe5e5b
Mr. Saga <114266859+NerdSaga@users.noreply.github.com>
2023-08-17 16:32:28 -0700

    Added files.

---
+++ ProGFX/rendering.js
@@ -0,0 +1,198 @@
+
+/** @type {HTMLCanvasElement} */
+const cvs = document.getElementById("cvs")
+
+
+/** @type {CanvasRenderingContext2D} */
+const ctx = cvs.getContext("2d")
+
+let clearColor = "#000000"
+let fillColor  = "#ffffff"
+let zoom = 1
+
+// TODO: Finish this object:
+const render = {
+
+    
+    /**
+     * Sets the color that will clear the screen when calling renderer.clear()
+     * @param {String} color 
+     * */
+    setClearColor(color) {
+        clearColor = color
+    },
+
+    /** @returns {String} */
+    getClearColor() {
+        return clearColor
+    },
+
+    /**
+     * Sets the color that will be used to fill rectangles.
+     * @param {String} color 
+     * */
+    setFillColor(color) {
+        fillColor = color
+    },
+
+    /** @returns {String} */
+    getFillColor() {
+        return fillColor
+    },
+
+
+    setZoom(zoomAmount) {
+        zoom = zoomAmount
+    },
+
+    /** @returns {number} */
+    getZoom() {
+        return zoom
+    },
+
+    /**
+     * @description Clears the screen.
+     */
+    clear() {
+        ctx.fillStyle = clearColor
+        ctx.fillRect(-cvs.width * 0.5, -cvs.height * 0.5, cvs.width, cvs.height)
+    },
+
+    
+
+    /** 
+     * @description Draws images using position, width, height, offset, and rotation.
+     * @param {HTMLImageElement} image  
+     * @param {number} x
+     * @param {number} y 
+     * @param {number} w 
+     * @param {number} h 
+     * @param {number} oX 
+     * @param {number} oY 
+     * @param {number} rot
+    */
+    drawImage(image, x, y, w, h, oX, oY, rot) {
+
+        switch (arguments.length) {
+            // Draw normal.
+            case 3:
+                ctx.drawImage(image, x, y)
+                break
+            
+            // Draw with w, and h.
+            case 5:
+                x *= zoom
+                y *= zoom
+                w *= zoom
+                h *= zoom
+                ctx.drawImage(image, x, y, w, h)
+
+            // Draw with offset.
+            case 7:
+                x += oX
+                y += oY
+                x *= zoom
+                y *= zoom
+                w *= zoom
+                h *= zoom
+                ctx.drawImage(image, x, y, w, h)
+                break
+
+            // Draw with rotation.
+            case 8:
+                x *= zoom
+                y *= zoom
+                w *= zoom
+                h *= zoom
+                oX *= zoom
+                oY *= zoom
+                ctx.save()
+                ctx.translate(x, y)
+                ctx.rotate(rot)
+                ctx.drawImage(image, oX, oY, w, h)
+                ctx.restore()
+                break
+
+            
+            // Warn.
+            default:
+                console.log("WARN: drawImage() incorrect number of arguments: " + arguments.length)
+        }
+
+    },
+
+
+    /**
+     * @description Draws rectangles using position, width, height, offset, and rotation.
+     * @param {number} x 
+     * @param {number} y 
+     * @param {number} w 
+     * @param {number} h 
+     * @param {number} oX
+     * @param {number} oY 
+     * @param {number} rot 
+     */
+    fillRect(x, y, w, h, oX, oY, rot) {
+        ctx.fillStyle = fillColor
+
+        switch (arguments.length) {
+            // Draw normal.
+            case 4:
+                x *= zoom
+                y *= zoom
+                w *= zoom
+                h *= zoom
+                ctx.fillRect(x, y, w, h)
+                break
+
+
+            // Draw with offset.
+            case 6:
+                x += oX
+                y += oY
+                x *= zoom
+                y *= zoom
+                w *= zoom
+                h *= zoom
+                ctx.fillRect(x, y, w, h)
+                break
+
+            // Draw with rotation.
+            case 7:
+                x *= zoom
+                y *= zoom
+                w *= zoom
+                h *= zoom
+                oX *= zoom
+                oY *= zoom
+                ctx.save()
+                ctx.translate(x, y)
+                ctx.rotate(rot)
+                ctx.fillRect(oX, oY, w, h)
+                ctx.restore()
+                break
+
+            
+            // Warn.
+            default:
+                console.log("WARN: drawRect() incorrect number of arguments: " + arguments.length)
+        }
+    }
+}
+
+// Setup
+cvs.width  = window.innerWidth
+cvs.height = window.innerHeight
+ctx.translate(cvs.width * 0.5, cvs.height * 0.5)
+
+
+// Events
+window.addEventListener("resize", function() {
+    cvs.width  = this.window.innerWidth
+    cvs.height = this.window.innerHeight
+    ctx.translate(cvs.width * 0.5, cvs.height * 0.5)
+    render.clear()
+})
+
+
+export { render, cvs, ctx }
---
+++ program.js
@@ -0,0 +1,114 @@
+import { render, ctx, cvs } from "./ProGFX/rendering.js"
+
+
+
+let images = []
+
+images.push(new Image())
+images[0].src = "./Images/Buddy.png"
+
+images.push(new Image())
+images[1].src = "./Images/Caramel.png"
+
+images.push(new Image())
+images[2].src = "./Images/Dexter.png"
+
+images.push(new Image())
+images[3].src = "./Images/Grumpo.png"
+
+images.push(new Image())
+images[4].src = "./Images/Radish.png"
+
+// images.push(new Image())
+// images[5].src = "./Images/Buddy-Boxed.png"
+
+
+render.setClearColor("#330033")
+
+let currentImage = 0
+let rot = 0
+let requiredFrames = 12
+let frames = 0
+let zoom = 1
+
+function update() {
+
+
+    render.clear()
+
+    if (frames >= requiredFrames) {
+        currentImage++
+        if (currentImage > images.length - 1) {
+            currentImage = 0
+        }
+        frames = 0
+    }
+
+    render.setZoom(cvs.height / 950)
+    render.setFillColor("#ffffff22")
+    render.fillRect(0, 0, 700, 700, -350, -350, rot)
+
+    zoom = (Math.cos(Date.now() * 0.002) + 4) * 0.2
+    render.setZoom(cvs.height / 350 * zoom)
+
+    render.setFillColor("#29002288")
+    render.fillRect(0, 0, 200, 200, -100, -100, -rot * 0.5)
+
+    render.setFillColor("#22002855")
+    render.fillRect(-250, 0, 200, 200, -100, -100, -rot * 0.5)
+
+    render.setFillColor("#22002855")
+    render.fillRect(250, 0, 200, 200, -100, -100, -rot * 0.5)
+
+
+    render.drawImage(images[currentImage], 0, 0, 150, 150, -75, -75, rot)
+    render.drawImage(images[currentImage], -250, 0, 150, 150, -75, -75, rot)
+    render.drawImage(images[currentImage], 250, 0, 150, 150, -75, -75, rot)
+
+    ctx.beginPath()
+    ctx.fillStyle = "#4444aa"
+    ctx.arc(cirLoc[0], cirLoc[1], 50, 0, Math.PI * 2, false)
+    ctx.fill()
+    ctx.closePath()
+    
+
+    rot += 0.05
+
+
+
+
+
+    frames++
+
+}
+
+var cirLoc = []
+cvs.addEventListener("mousemove", (ev) => {
+    cirLoc[0] = ev.clientX + -cvs.width * 0.5
+    cirLoc[1] = ev.clientY + -cvs.height * 0.5
+})
+
+
+let fps = 45
+let requiredElapsed = 1000 / fps
+let elapsed = 0
+let now = 0
+let then = Date.now()
+
+
+function loop() {
+    now = Date.now()
+    elapsed = now - then
+    if (elapsed > requiredElapsed) {
+        update()
+        
+        then = now
+    }
+    requestAnimationFrame(loop)
+}
+
+// ctx.scale(cvs.height / 350, cvs.height / 350)
+window.addEventListener("resize", function() {
+    // ctx.scale(cvs.height / 350, cvs.height / 350)
+})
+requestAnimationFrame(loop)
