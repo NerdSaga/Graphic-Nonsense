268507723e2caf02476af393fd399e3e17dcc0ae
Brennan Sage <brennansage01@gmail.com>
2024-04-16 13:48:13 -0700

    Made to PWA!

---
+++ serviceWorker.js
@@ -0,0 +1,24 @@
+const gameConsole = "game-console"
+const assets = [
+    "/",
+    "/app.js",
+    "/index.html",
+    "/styles.css",
+    "/program.js"
+]
+
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches.open(gameConsole).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        caches.match(e.request).then(res => {
+            return res || fetch(e.request)
+        })
+    )
+})
